99570e83c06e20c7b01b110de85e06996e400048
Diego Eckhard <diego@eckhard.com.br>
2017-06-22 10:26:23 -0300

    Change Euler and Runge-Kutta to semi-batch simulations

--- solver/bio_euler.m
+++ solver/bio_euler.m
@@ -1,9 +1,24 @@
-function [x,t]=bio_euler(f,ti,xi,h,N)
-%[x,t]=bio_euler(f,ti,xi,h,N)
-t=(ti:h:ti+(N-1)*h);
-x=xi;
-for i=1:N-1
-    k1=f(x(:,i),t(i));
-    x(:,i+1)=x(:,i)+h*k1;
-end
+function [x,t]=bio_runge_4(f,ti,xi,h,N)
+%[x,t]=bio_runge_4(f,ti,xi,h,N)
 
+% Time vector
+t=(ti:h:ti+(sum(N)-1)*h);
+
+% Reduce last N to ensure t has same dimension that x
+N(end)=N(end)-1;
+
+x=zeros(size(xi,1),1);
+
+i=1;
+for j=1:length(N)
+
+    % Add value to states
+    x(:,i)=x(:,i)+xi(:,j);
+
+    for k=1:N(j)
+        k1=f(x(:,i),t(i));
+        x(:,i+1)=x(:,i)+h*k1;
+        i=i+1;
+    end
+    
+end
--- solver/bio_runge_4.m
+++ solver/bio_runge_4.m
@@ -1,12 +1,27 @@
 function [x,t]=bio_runge_4(f,ti,xi,h,N)
 %[x,t]=bio_runge_4(f,ti,xi,h,N)
 
-t=(ti:h:ti+(N-1)*h);
-x=xi;
-for i=1:N-1
-    k1=f(x(:,i),t(i));
-    k2=f(x(:,i)+h*k1/2,t(i)+h/2);
-    k3=f(x(:,i)+h*k2/2,t(i)+h/2);
-    k4=f(x(:,i)+h*k3,t(i)+h);
-    x(:,i+1)=x(:,i)+h/6*(k1+2*k2+2*k3+k4);
+% Time vector
+t=(ti:h:ti+(sum(N)-1)*h);
+
+% Reduce last N to ensure t has same dimension that x
+N(end)=N(end)-1;
+
+x=zeros(size(xi,1),1);
+
+i=1;
+for j=1:length(N)
+
+    % Add value to states
+    x(:,i)=x(:,i)+xi(:,j);
+
+    for k=1:N(j)
+        k1=f(x(:,i),t(i));
+        k2=f(x(:,i)+h*k1/2,t(i)+h/2);
+        k3=f(x(:,i)+h*k2/2,t(i)+h/2);
+        k4=f(x(:,i)+h*k3,t(i)+h);
+        x(:,i+1)=x(:,i)+h/6*(k1+2*k2+2*k3+k4);
+        i=i+1;
+    end
+    
 end
