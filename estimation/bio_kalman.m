33abb67996485dfd38b685dff2582a1c94dc0e70
emersonboeira <emerson.boeira@gmail.com>
2017-07-06 17:51:30 -0300

    Update bio_kalman.m

--- estimation/bio_kalman.m
+++ estimation/bio_kalman.m
@@ -3,14 +3,17 @@ function [XC]=bio_kalman(F,B,H,xc,Q,R,P,u,z)
 N=length(z);
 XC=[xc];
 
-for i=1:(N-1)
-    xc=F*xc+B*u(:,i);
-    P=F*P*F'+Q;
-    y=z(i+1)-H*xc
+for i=1:(N)
+    %a posteriori
+    y=z(i)-H*xc;
     S=H*P*H'+R;
     K=P*H'*inv(S);
     xc=xc+K*y;
     P=(eye(2)-K*H)*P;
     XC=[XC xc];
+    %a priori
+    xc=F*xc+B*u(:,i);
+    P=F*P*F'+Q;
 end
-
+XC=XC(:,2:end); %estimativa a posteriori
+end
