f3a75c995087238aa533d23f6d9c74e88a1a8d3b
Diego Eckhard <diegoeck@ufrgs.br>
2018-08-15 12:43:14 -0300

    Coloca dados de 3 em 3 horas

--- data/glicerina/converteCSV.m
+++ data/glicerina/converteCSV.m
@@ -3,29 +3,55 @@ clear all
 %Leitura de dados do arquivo
 disp('Aguarde processamento...'); 
 fileID = fopen('reactor3.csv');
-C = textscan(fileID,'%d %{dd/MM/yyyy HH:mm:SS}D %f %f %f','Delimiter',',');
+C1 = textscan(fileID,'%d %{dd/MM/yyyy HH:mm:SS}D %f %f %f','Delimiter',',');
 fclose(fileID);
+
+
+
+fileID = fopen('glicerina.csv');
+C2 = textscan(fileID,'%d %{dd/MM/yyyy HH:mm:SS}D %f','Delimiter',',');
+fclose(fileID);
+
+
+
 disp('1)Leitura do arquivo: ok');
 
-N = length(C{1});
+N1 = length(C1{1});
+d1=C1{2}
+v1=C1{5}
 
-d=C{2}
-v=C{5}
+N2 = length(C2{1});
+d2=C2{2}
+v2=C2{3}
 
-T=8
 
 
-M=ceil(datenum(d(end)-d(1))*24/T);
+T=3
+
+
+M=ceil(datenum(d1(end)-d1(1))*24/T);
 
 volume=zeros(M,1);
+glicerina=zeros(M,1);
 
 disp('2)Pre-processamento do arquivo fonte: ok');
-for i=1:N
-    t=floor(datenum(d(i)-d(1))*24/T)+1;
-    volume(t)=volume(t)+v(i);
+for i=1:N1
+    t=floor(datenum(d1(i)-d1(1))*24/T)+1;
+    volume(t)=volume(t)+v1(i);
 end
+for i=1:N2
+    t=floor(datenum(d2(i)-d1(1))*24/T)+1;
+    glicerina(t)=glicerina(t)+v2(i);
+end
+
+
+
 
 disp('3)Grafico: ok');
 figure(1)
 stairs(volume)
+hold on
+stairs(glicerina*30)
+hold off
+
 
--- data/glicerina/plota_dados.m
+++ data/glicerina/plota_dados.m
@@ -4,12 +4,73 @@ load('dados2.mat')
 v2=volume;
 load('dados3.mat')
 v3=volume;
+load('glicerina.mat')
+g=glicerina;
+
+N=length(g);
 
 figure(1)
-plot(v1)
+stairs(v1)
 hold on
-plot(v2)
-plot(v3)
+stairs(g*30)
+hold off
 
+figure(2)
+stairs(v2)
+hold on
+stairs(g*30)
+hold off
+
+figure(3)
+stairs(v3)
+hold on
+stairs(g*30)
+hold off
+
+
+figure(11)
+T=cumsum(v1);
+T=T-T(203);
+Tm=max(T);
+stairs(T)
+hold on
+stairs(g*300)
+plot([1 N],[Tm Tm])
+plot([1 N],[Tm Tm]/12*1)
+plot([1 N],[Tm Tm]/12*2)
+plot([1 N],[Tm Tm]/12*3)
+plot([1 N],[Tm Tm]/12*5)
+plot([1 N],[Tm Tm]/12*8)
+hold off
+
+figure(12)
+T=cumsum(v2);
+T=T-T(203);
+Tm=max(T);
+stairs(T)
+hold on
+stairs(g*300)
+plot([1 N],[Tm Tm])
+plot([1 N],[Tm Tm]/12*1)
+plot([1 N],[Tm Tm]/12*2)
+plot([1 N],[Tm Tm]/12*3)
+plot([1 N],[Tm Tm]/12*5)
+plot([1 N],[Tm Tm]/12*8)
+hold off
+
+figure(13)
+T=cumsum(v3);
+T=T-T(203);
+Tm=max(T);
+stairs(T)
+hold on
+stairs(g*300)
+plot([1 N],[Tm Tm])
+plot([1 N],[Tm Tm]/12*1)
+plot([1 N],[Tm Tm]/12*2)
+plot([1 N],[Tm Tm]/12*3)
+plot([1 N],[Tm Tm]/12*5)
+plot([1 N],[Tm Tm]/12*8)
+hold off
 
 
